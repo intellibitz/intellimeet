be5943c5366479e4559f27fe334807cef149fd87
muthu.ramadoss <muthu.ramadoss@1698c8b9-0341-0410-8d42-7df82a121400>
2008-01-23 09:04:03 +0000

    Added 'User' Entity and tests.

--- trunk/32tango/intellimeet-ejb/src/test/java/com/ibt/intellimeet/dummy/DummyUserTest.java
+++ trunk/32tango/intellimeet-ejb/src/test/java/com/ibt/intellimeet/dummy/DummyUserTest.java
@@ -1,12 +1,6 @@
 package com.ibt.intellimeet.dummy;
 
-import org.jboss.deployers.spi.DeploymentException;
-import org.jboss.embedded.Bootstrap;
-import org.jboss.seam.mock.SeamTest;
-import org.jboss.virtual.plugins.context.vfs.AssembledContextFactory;
-import org.jboss.virtual.plugins.context.vfs.AssembledDirectory;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import com.ibt.intellimeet.ejb.EJBTest;
 import org.testng.annotations.Test;
 
 import javax.naming.InitialContext;
@@ -14,169 +8,22 @@ import javax.persistence.EntityManager;
 import javax.transaction.TransactionManager;
 import java.util.logging.Logger;
 
-/*
-import junit.framework.TestSuite;
-import junit.framework.TestCase;
-import junit.extensions.TestSetup;
-*/
-
-
 /**
  * DummyUserTest Unit test for the DummyUser Entity
  */
 public class DummyUserTest
-        extends SeamTest
+        extends EJBTest
 {
+
+    Logger log = Logger.getLogger("DummyUserTest");
 
     public DummyUserTest()
     {
         super();
     }
 
-    @Override
-    protected void startJbossEmbeddedIfNecessary()
-            throws Exception
-    {
-    }
-
-    Logger log = Logger.getLogger("DummyUserTest");
-
-    private static AssembledDirectory jar;
-    private static boolean globalSetup = false;
-
-    private static void deploy()
-    {
-        jar = AssembledContextFactory.getInstance()
-                .create("intellimeet-ejb.jar");
-        jar.addClass(DummyUser.class);
-        jar.addClass(IDummyUserLocal.class);
-        jar.addClass(DummyUserAction.class);
-        AssembledDirectory assembledDirectory = jar.mkdir("META-INF");
-        assembledDirectory.addResource("META-INF/persistence.xml");
-        assembledDirectory.addResource("META-INF/components.xml");
-        try
-        {
-            Bootstrap bootstrap = Bootstrap.getInstance();
-/*
-            if (resourceExists("seam.properties")) {
-                bootstrap.deployResourceBases("seam.properties");
-            }
-*/
-            if (resourceExists("META-INF/components.xml"))
-            {
-                bootstrap.deployResourceBases("META-INF/components.xml");
-            }
-/*
-            if (resourceExists("META-INF/seam.properties")) {
-                bootstrap.deployResourceBases("META-INF/seam.properties");
-            }
-*/
-            bootstrap.deploy(jar);
-        }
-        catch (DeploymentException e)
-        {
-            throw new RuntimeException("Unable to deploy", e);
-        }
-    }
-
-
-    private static boolean resourceExists(String name)
-    {
-        return Thread.currentThread().getContextClassLoader()
-                .getResource(name) != null;
-    }
-
-    private static void undeploy()
-    {
-        try
-        {
-            Bootstrap.getInstance().undeploy(jar);
-            AssembledContextFactory.getInstance().remove(jar);
-        }
-        catch (DeploymentException e)
-        {
-            throw new RuntimeException("Unable to undeploy", e);
-        }
-    }
-
-/*
-    public static junit.framework.Test suite()
-    {
-       TestSuite suite = new TestSuite();
-       suite.addTestSuite(DummyUserTest.class);
-       globalSetup = true;
-
-       return new TestSetup(suite)
-       {
-          @Override
-          protected void setUp() throws Exception
-          {
-             super.setUp();
-             if (!Bootstrap.getInstance().isStarted())
-             {
-                Bootstrap.getInstance().bootstrap();
-             }
-             deploy();
-          }
-
-          @Override
-          protected void tearDown() throws Exception
-          {
-             undeploy();
-             if (System.getProperty("shutdown.embedded.jboss") != null) Bootstrap.getInstance().shutdown();
-             super.tearDown();
-          }
-       };
-    }
-*/
-
-    //    @Override
-
-    @BeforeSuite
-    protected void setUp()
-            throws Exception
-    {
-        if (globalSetup)
-        {
-            return;
-        }
-        Bootstrap.getInstance().bootstrap();
-        deploy();
-        log.info("Embedded Jboss deploy - SUCCESS");
-    }
-
-    //    @Override
-    @AfterSuite
-    protected void tearDown()
-            throws Exception
-    {
-        if (globalSetup)
-        {
-            return;
-        }
-        undeploy();
-        log.info("Embedded Jboss undeploy - SUCCESS");
-    }
-
-/*
-    @Configuration (beforeSuite = true)
-    protected void setUp() throws Exception
-    {
-       if (globalSetup) return;
-       Bootstrap.getInstance().bootstrap();
-       deploy();
-    }
-
-    @Configuration (afterSuite = true)
-    protected void tearDown() throws Exception
-    {
-        if (globalSetup) return;
-        undeploy();
-    }
-*/
-
     @Test
-    public void testEntityManager()
+    public void testDummyUser()
             throws Exception
     {
 
