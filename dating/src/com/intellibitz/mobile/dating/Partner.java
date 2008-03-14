a17486921df87ab2d0ea4af6074b69a00eddc0a3
archana.14nair <archana.14nair@1698c8b9-0341-0410-8d42-7df82a121400>
2008-03-14 13:57:01 +0000

    Spinner included

--- dating/src/com/intellibitz/mobile/dating/Partner.java
+++ dating/src/com/intellibitz/mobile/dating/Partner.java
@@ -1,40 +1,80 @@
-/**
- * 
- */
+
 package com.intellibitz.mobile.dating;
+
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.content.Intent;
 
-/**
- * @author gunasekaran
- *
- */
+
 public class Partner extends Activity {
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        //setTheme(android.R.style.Theme_Light);
+
         setContentView(R.layout.partner);
-        Button activatebutton = (Button) findViewById(R.id.send);
+
+        Spinner s1 = (Spinner) findViewById(R.id.agespinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.age, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(adapter);
+
+
+        Spinner s2 = (Spinner) findViewById(R.id.heightspinner2);
+        adapter = ArrayAdapter.createFromResource(this, R.array.height,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s2.setAdapter(adapter);
+
+        Spinner s3 = (Spinner) findViewById(R.id.weightspinner3);
+        adapter = ArrayAdapter.createFromResource(this, R.array.weight,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s3.setAdapter(adapter);
+
+        Spinner s4 = (Spinner) findViewById(R.id.locnspinner4);
+        adapter = ArrayAdapter.createFromResource(this, R.array.location,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s4.setAdapter(adapter);
+
+        Button sendbutton = (Button) findViewById(R.id.send);
+        if(sendbutton!=null)
+        {
+        sendbutton.setOnClickListener(new Button.OnClickListener(){
+        public void onClick(View V)
+        	{
+
+
+             Intent intobj = new Intent(Partner.this,DatingServiceController.class);
+             startActivity(intobj);
+
+            }
+        });
+        }
+
         Button backbutton = (Button) findViewById(R.id.back);
-        activatebutton.setOnClickListener(new Button.OnClickListener() {
+        if(backbutton!=null)
+        {
+        backbutton.setOnClickListener(new Button.OnClickListener(){
+        public void onClick(View V)
+        	{
 
-            public void onClick(View v) {
-                Intent myIntent1 = new Intent(Partner.this, DatingServiceController.class);
-                startActivity(myIntent1);
+
+             Intent intobj = new Intent(Partner.this,Seeker.class);
+             startActivity(intobj);
+
             }
         });
-        
-        backbutton.setOnClickListener(new Button.OnClickListener() {
-
-            public void onClick(View v) {
-                Intent intent = new Intent(Partner.this, Seeker.class);
-                startActivity(intent);
-            }
-        });
+        }
+      }
     }
-}
+
+
