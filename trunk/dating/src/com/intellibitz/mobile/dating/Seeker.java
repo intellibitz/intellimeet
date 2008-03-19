f76916b62078001711cde24306144c948862e3eb
nithin.warier <nithin.warier@1698c8b9-0341-0410-8d42-7df82a121400>
2008-03-19 14:10:46 +0000

    Create Database, tables to store the seeker profile and dream-date profile

--- trunk/dating/src/com/intellibitz/mobile/dating/Seeker.java
+++ trunk/dating/src/com/intellibitz/mobile/dating/Seeker.java
@@ -1,20 +1,22 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.intellibitz.mobile.dating;
 
 import android.app.Activity;
-import android.app.AlarmManager;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
 
 /**
  *
@@ -23,95 +25,197 @@ import android.widget.Toast;
 public class Seeker extends Activity {
 
     /** Called when the activity is first created. */
+    int count = 0;
+    SQLiteDatabase myDatabase = null;
+    
+    String getSeekerAge;
+    String getSeekerSex;
+    String getSeekerHeight;
+    String getSeekerWeight;
+    String getSeekerLocation;
+    
+    String getPartnerAge;
+    String getPartnerSex;
+    String getPartnerHeight;
+    String getPartnerWeight;
+    String getPartnerLocation;
+    
+    TextView ageTextView;
+    TextView heightTextView;
+    TextView weightTextView;
+    TextView locationTextView;
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        
         // ToDo add your GUI initialization code here 
         setContentView(R.layout.main);
-
-        TabHost tabs = (TabHost) this.findViewById(R.id.tabs);
+        final TabHost tabs = (TabHost) this.findViewById(R.id.tabs);
         tabs.setup();
-
         TabHost.TabSpec one = tabs.newTabSpec("one");
         one.setContent(R.id.yourProfile);
+        try {
+             this.createDatabase("Dating", 1, MODE_PRIVATE, null);
+             myDatabase = this.openDatabase("Dating", null);
+            }
+        catch(FileNotFoundException e) {}
+        RadioButton seekerMale = (RadioButton)findViewById(R.id.seekermaleradiobutton);
+        seekerMale.setChecked(true);
         one.setIndicator("Your's", this.getResources().getDrawable(R.drawable.yourprofile));
+        String[] COUNTRIES = new String[] {
+        "Uttar Pradesh","Maharashtra","Bihar","West Bengal","Andhra Pradesh","Tamil Nadu","Madhya Pradesh",
+        "Rajasthan","Karnataka","Gujarat","Orissa","Kerala","Jharkhand","Assam","Punjab","Haryana","Chhattisgarh",
+        "Delhi","Jammu and Kashmir","Uttarakhand ","Himachal Pradesh","Tripura","Manipur","Meghalaya","Nagaland",
+        "Goa","Arunachal Pradesh","Pondicherry","Chandigarh","Mizoram","Sikkim","Andaman and Nicobar Islands",
+        "Dadra and Nagar Haveli","Daman and Diu","Lakshadweep"};
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.Location);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, COUNTRIES);
+        textView.setAdapter(adapter1);
         tabs.addTab(one);
-
         TabHost.TabSpec two = tabs.newTabSpec("two");
         two.setContent(R.id.partner);
         two.setIndicator("Partner", this.getResources().getDrawable(R.drawable.partnerprofile));
-        tabs.addTab(two);
-
-        Spinner s1 = (Spinner) findViewById(R.id.agespinner1);
+        final Spinner s1 = (Spinner) findViewById(R.id.agespinner1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.age, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s1.setAdapter(adapter);
-
-        Spinner s2 = (Spinner) findViewById(R.id.heightspinner2);
+        s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            
+            public void onItemSelected(AdapterView parent, View v, int position, long id) {
+                 getPartnerAge=(String) s1.getSelectedItem();
+                 if(getPartnerAge.equals("Select Age"))
+                    getPartnerAge = "null";
+            }
+        public void onNothingSelected(AdapterView parent) {
+            }
+          });
+        final Spinner s2 = (Spinner) findViewById(R.id.heightspinner2);
         adapter = ArrayAdapter.createFromResource(this, R.array.height,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s2.setAdapter(adapter);
-
-        Spinner s3 = (Spinner) findViewById(R.id.weightspinner3);
+        s2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView parent, View v, int position, long id) {
+                getPartnerHeight = (String)s2.getSelectedItem();
+                if(getPartnerHeight.equals("Select Height"))
+                    getPartnerHeight = "null";
+            }
+           public void onNothingSelected(AdapterView parent) {
+            }
+        });
+        
+        final Spinner s3 = (Spinner) findViewById(R.id.weightspinner3);
         adapter = ArrayAdapter.createFromResource(this, R.array.weight,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s3.setAdapter(adapter);
-
-        Spinner s4 = (Spinner) findViewById(R.id.locnspinner4);
+        s3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView parent, View v, int position, long id) {
+                getPartnerWeight = (String)s3.getSelectedItem();
+                if(getPartnerWeight.equals("Select Weight"))
+                    getPartnerWeight = "null";
+               }
+            public void onNothingSelected(AdapterView parent) {
+            }
+        });
+        
+        final Spinner s4 = (Spinner) findViewById(R.id.locnspinner4);
         adapter = ArrayAdapter.createFromResource(this, R.array.location,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s4.setAdapter(adapter);
-
+        s4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView parent, View v, int position, long id) {
+                getPartnerLocation = (String)s4.getSelectedItem();
+                if(getPartnerLocation.equals("Select Location"))
+                           getPartnerLocation = "null";
+            }
+            public void onNothingSelected(AdapterView parent) {
+            }
+        });
+        RadioButton partnerSex = (RadioButton)findViewById(R.id.partnerfemaleradiobutton);
+        partnerSex.setChecked(true);
+        if(partnerSex.isChecked() == true)
+            getPartnerSex = "female";
+        else
+            getPartnerSex = "male";
+        
+        tabs.addTab(two);
         TabHost.TabSpec three = tabs.newTabSpec("three");
         three.setContent(R.id.serviceActivate);
         three.setIndicator("Service", this.getResources().getDrawable(R.drawable.chat));
         tabs.addTab(three);
-
-        Button button = (Button) findViewById(R.id.Activate);
+        final Button button = (Button) findViewById(R.id.Activate);
         button.setOnClickListener(new Button.OnClickListener() {
-
-            private int count;
-
-            public void onClick(View v) {
+        public void onClick(View v) {
+                if(v.getId() == (R.id.Activate))   {
                 if (count == 0) {
+                       ageTextView = (TextView) findViewById(R.id.Age);
+                    heightTextView = (TextView) findViewById(R.id.Height);
+                    weightTextView = (TextView) findViewById(R.id.Weight);
+                    locationTextView = (TextView)findViewById(R.id.Location);
+                    RadioButton seekerFemale = (RadioButton)findViewById(R.id.seekerfemaleradiobutton);
+                    if(seekerFemale.isChecked() == true) 
+                        getSeekerSex = "female";
+                    else
+                        getSeekerSex = "male";
+                    getSeekerAge = ageTextView.getText().toString();
+                    getSeekerHeight = heightTextView.getText().toString();
+                    getSeekerWeight = weightTextView.getText().toString();
+                    getSeekerLocation = locationTextView.getText().toString();
+                    if(getSeekerAge.equals(""))
+                                getSeekerAge = "0";
+                    if(getSeekerHeight.equals(""))
+                               getSeekerHeight = "0";
+                    if(getSeekerWeight.equals(""))
+                                getSeekerWeight = "0";
+                    if(getSeekerLocation.equals(""))
+                                getSeekerLocation = "null";
+                     myDatabase.execSQL("CREATE TABLE IF NOT EXISTS Seeker"
+                                   +" (seekerAge INT(3), seekerSex VARCHAR, seekerHeight INT(3), " +
+                                   "seekerWeight INT(3), seekerLocation VARCHAR);");
+                     myDatabase.execSQL("INSERT INTO Seeker (seekerAge, seekerSex, seekerHeight, seekerWeight, " +
+                                   "seekerLocation) VALUES ("+getSeekerAge+",'"+getSeekerSex+"',"+getSeekerHeight
+                                               +","+getSeekerWeight+",'"+getSeekerLocation+"');"); 
+                     myDatabase.execSQL("CREATE TABLE IF NOT EXISTS Partner"
+                                   +" (partnerAgeRange VARCHAR, partnerSex VARCHAR, partnerHeightRange VARCHAR," +
+                                   "partnerWeightRange VARCHAR, partnerLocation VARCHAR);");
+                             
+                            myDatabase.execSQL("INSERT INTO Partner (partnerAgeRange, partnerSex, partnerHeightRange," +
+                                    "partnerWeightRange, partnerLocation) VALUES ('"+getPartnerAge+"','"+getPartnerSex+
+                                    "','"+getPartnerHeight+"','"+getPartnerWeight+"','"+getPartnerLocation+"');");
+                     
                     startService(new Intent(Seeker.this, DatingService.class), null);
                     count++;
-                    //butActivate.setVisibility(Button.INVISIBLE);
-                    Intent intobject1 = new Intent(Seeker.this, RepeatingAlarm.class);
                     Intent intobject = new Intent(Seeker.this, MatchingData.class);
                     startActivity(intobject);
-
-                    long firstTime = SystemClock.elapsedRealtime();
-                    firstTime += 10 * 1000;
-
-                    // Schedule the alarm!
-                    AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
-                    am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                            firstTime, 10 * 1000, intobject1);
-
                     // Tell the user about what we did.
                     if (mToast != null) {
                         mToast.cancel();
-                    }
+                  }
                     mToast = Toast.makeText(Seeker.this, R.string.repeating_received,
                             Toast.LENGTH_LONG);
                     mToast.show();
-
-                } else {
+                } 
+              }
+              else {
                     Toast.makeText(Seeker.this, "Service already activated", Toast.LENGTH_SHORT).show();
-                }
+                   }
             }
-            Toast mToast;
+           Toast mToast;
         });
-
         TabHost.TabSpec four = tabs.newTabSpec("four");
         four.setContent(R.id.help);
         four.setIndicator("Help", this.getResources().getDrawable(R.drawable.help));
         tabs.addTab(four);
         tabs.setCurrentTab(0);
-    }
-}
+    }   
+  }
+     
+
+  
+
+
+
