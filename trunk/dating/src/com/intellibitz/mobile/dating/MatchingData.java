0573a0e174dc78055afa9b48872c749d2f164468
gunaztar <gunaztar@1698c8b9-0341-0410-8d42-7df82a121400>
2008-03-31 06:28:49 +0000

    HitEvent added

--- trunk/dating/src/com/intellibitz/mobile/dating/MatchingData.java
+++ trunk/dating/src/com/intellibitz/mobile/dating/MatchingData.java
@@ -2,6 +2,8 @@ package com.intellibitz.mobile.dating;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
@@ -16,6 +18,8 @@ import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import android.view.Menu;
 import android.view.Menu.Item;
+import android.widget.Toast;
+import com.google.android.maps.MapView.DeviceType;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayController;
 import com.google.android.maps.Point;
@@ -29,6 +33,8 @@ public class MatchingData extends MapActivity {
     private MapView mMapView;
     private ViewFlipper mFlipper;
     private Point p1,  p2,  p3,  p4;
+    private Bitmap bubbleIcon;
+    private Bitmap bubbleIcon1;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -37,7 +43,8 @@ public class MatchingData extends MapActivity {
         setContentView(R.layout.matchingprofile);
         mFlipper = ((ViewFlipper) this.findViewById(R.id.flipper));
         mFlipper.startFlipping();
-
+        bubbleIcon = BitmapFactory.decodeResource(getResources(),R.drawable.bubble);
+        bubbleIcon1 = BitmapFactory.decodeResource(getResources(),R.drawable.bubble1);
         Button button = (Button) findViewById(R.id.chating);
         mMapView = (MapView) findViewById(R.id.map_whereami);
         p1 = new Point((int) (13098200), (int) (80225937));
@@ -80,7 +87,8 @@ public class MatchingData extends MapActivity {
             c.drawText("you", coords[0] + 9, coords[1], paint);
             paint.setARGB(200, 34, 234, 34);
             //paint.setStrokeWidth(1);
-            c.drawOval(oval, paint);
+            c.drawBitmap(bubbleIcon, coords[0] - bubbleIcon.width() / 2, coords[1] - bubbleIcon.height(), null);
+            //c.drawOval(oval, paint);
 
             calc.getPointXY(p2, coords1);
             RectF oval1 = new RectF(coords1[0] - 7, coords1[1] + 7, coords1[0] + 7, coords1[1] - 7);
@@ -94,7 +102,8 @@ public class MatchingData extends MapActivity {
             c.drawLine(coords[0], coords[1], coords1[0], coords1[1], paint1);
             paint1.setStrokeWidth(1);
             paint1.setARGB(80, 255, 0, 0);
-            c.drawOval(oval1, paint1);
+            c.drawBitmap(bubbleIcon1, coords1[0] - bubbleIcon.width() / 2, coords1[1] - bubbleIcon.height(), null);
+            //c.drawOval(oval1, paint1);
 
             calc.getPointXY(p3, coords2);
             RectF oval2 = new RectF(coords2[0] - 7, coords2[1] + 7, coords2[0] + 7, coords2[1] - 7);
@@ -108,7 +117,8 @@ public class MatchingData extends MapActivity {
             c.drawLine(coords[0], coords[1], coords2[0], coords2[1], paint2);
             paint2.setStrokeWidth(1);
             paint2.setARGB(80, 255, 0, 0);
-            c.drawOval(oval2, paint2);
+            c.drawBitmap(bubbleIcon1, coords2[0] - bubbleIcon.width() / 2, coords2[1] - bubbleIcon.height(), null);
+            //c.drawOval(oval2, paint2);
 
             calc.getPointXY(p4, coords3);
             RectF oval3 = new RectF(coords3[0] - 7, coords3[1] + 7, coords3[0] + 7, coords3[1] - 7);
@@ -122,16 +132,70 @@ public class MatchingData extends MapActivity {
             c.drawLine(coords[0], coords[1], coords3[0], coords3[1], paint3);
             paint3.setStrokeWidth(1);
             paint3.setARGB(80, 255, 0, 0);
-            c.drawOval(oval3, paint3);
+            c.drawBitmap(bubbleIcon1, coords3[0] - bubbleIcon.width() / 2, coords3[1] - bubbleIcon.height(), null);
+            //c.drawOval(oval3, paint3);
         }
+        @Override
+        public boolean onTap(DeviceType devicetype,Point p,PixelCalculator calculator){
+               this.getHitMapLocation(calculator,p);
+               return true;
+            }
+    private void  getHitMapLocation(PixelCalculator calculator, Point	tapPoint) {
+    	
+    	//  Track which MapLocation was hit...if any
+    	//MapLocation hitMapLocation = null;
+		
+                RectF hitTestRecr = new RectF();
+		int[] screenCoords = new int[2];
+	    	calculator.getPointXY(p1, screenCoords);
+    		hitTestRecr.set(-bubbleIcon1.width()/2,-bubbleIcon1.height(),bubbleIcon1.width()/2,0);
+    		hitTestRecr.offset(screenCoords[0],screenCoords[1]);
+    		calculator.getPointXY(tapPoint, screenCoords);
+    		if (hitTestRecr.contains(screenCoords[0],screenCoords[1])) {
+    			Toast.makeText(MatchingData.this, "This is your location. Click on dreammate for chat", Toast.LENGTH_LONG).show();
+    		
+    		}
+                RectF hitTestRecr1 = new RectF();
+		int[] screenCoords1 = new int[2];
+	    	calculator.getPointXY(p2, screenCoords1);
+    		hitTestRecr1.set(-bubbleIcon1.width()/2,-bubbleIcon1.height(),bubbleIcon1.width()/2,0);
+    		hitTestRecr1.offset(screenCoords1[0],screenCoords1[1]);
+    		calculator.getPointXY(tapPoint, screenCoords1);
+    		if (hitTestRecr1.contains(screenCoords1[0],screenCoords1[1])) {
+    		Intent intent=new Intent(MatchingData.this,Chat.class);
+                   startActivity(intent);
+                }
+                RectF hitTestRecr2 = new RectF();
+		int[] screenCoords2 = new int[2];
+	    	calculator.getPointXY(p3, screenCoords2);
+    		hitTestRecr2.set(-bubbleIcon1.width()/2,-bubbleIcon1.height(),bubbleIcon1.width()/2,0);
+    		hitTestRecr2.offset(screenCoords2[0],screenCoords2[1]);
+    		calculator.getPointXY(tapPoint, screenCoords2);
+    		if (hitTestRecr2.contains(screenCoords2[0],screenCoords2[1])) {
+    			Intent intent2=new Intent(MatchingData.this,Chat.class);
+                   startActivity(intent2);
+    		}
+                RectF hitTestRecr3 = new RectF();
+		int[] screenCoords3 = new int[2];
+	    	calculator.getPointXY(p4, screenCoords3);
+    		hitTestRecr3.set(-bubbleIcon1.width()/2,-bubbleIcon1.height(),bubbleIcon1.width()/2,0);
+    		hitTestRecr3.offset(screenCoords3[0],screenCoords3[1]);
+    		calculator.getPointXY(tapPoint, screenCoords3);
+    		if (hitTestRecr3.contains(screenCoords3[0],screenCoords3[1])) {
+    			Intent intent=new Intent(MatchingData.this,Chat.class);
+                   startActivity(intent);
+    		}
+    	}
     }
+    
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
+ public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_I) {
             // Zoom In
             int level = mMapView.getZoomLevel();
             mMapView.getController().zoomTo(level + 1);
+        
             return true;
         } else if (keyCode == KeyEvent.KEYCODE_O) {
             // Zoom Out
@@ -147,6 +211,7 @@ public class MatchingData extends MapActivity {
             mMapView.toggleTraffic();
             return true;
         }
+        
 
         return false;
     }
@@ -162,10 +227,12 @@ public class MatchingData extends MapActivity {
     public boolean onOptionsItemSelected(Item item) {
         switch (item.getId()) {
             case 1:
-                Intent intent = new Intent(MatchingData.this, Seeker.class);
+                Intent intent = new Intent(MatchingData.this, Dating.class);
                 startActivity(intent);
+                finish();
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
